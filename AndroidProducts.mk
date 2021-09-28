98f0399749105072d724562879cc0309cde95b31
ReallySnow <reallysnow233@gmail.com>
2021-09-28 09:16:48 +0000

    osborn: Lineage-ify
    Change-Id: I6b1c01a75b0b52a47f452faff76e8ae15ca6f3e1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018 The MoKee Open Source Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,4 +16,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/mokee_osborn.mk
+    $(LOCAL_DIR)/lineage_osborn.mk
